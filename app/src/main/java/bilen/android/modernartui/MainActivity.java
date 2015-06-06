6c7ee2c16fa96054c1a83c5a810feefff6cc5140
Bilen Öğretmen <ogretmen@gmail.com>
2015-06-06 19:26:01 +0300

    First commit...

--- app/src/main/java/bilen/android/modernartui/MainActivity.java
+++ app/src/main/java/bilen/android/modernartui/MainActivity.java
@@ -1,10 +1,20 @@
 package bilen.android.modernartui;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,35 +24,50 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
-    private TextView txt1, txt2, txt3, txt4,txt5;
+
     private SeekBar seekBar;
     private ArrayList<TextView> listToChange= new ArrayList<>();
-
+    // String for LogCat documentation
+    private final static String TAG = "ModernArtUI";
+    private final static int RGB_COLOR = 256;
+    //private int previousProgress = 0;
+    static private final String URL = "http://www.moma.org";
+    private DialogFragment mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt1 = (TextView) findViewById(R.id.textView1);
-        txt2 = (TextView) findViewById(R.id.textView2);
-        txt3 = (TextView) findViewById(R.id.textView3);
-        txt4 = (TextView) findViewById(R.id.textView4);
-        txt5 = (TextView) findViewById(R.id.textView5);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        listToChange.add(txt1);
-        listToChange.add(txt2);
-        listToChange.add(txt3);
-        listToChange.add(txt4);
-        listToChange.add(txt5);
 
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        ViewGroup viewGroup = (ViewGroup)findViewById(R.id.views);
+        listToChange = getTextViews(viewGroup);
+        //Log.i(TAG,"Selected List Size:"+ String.valueOf(listToChange.size()));
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 //Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
-                for (TextView txtview:listToChange)
-                {
-                    txtview.setBackgroundColor(((ColorDrawable)txtview.getBackground()).getColor() +1);
+                Log.i(TAG, String.valueOf(progress) + "->"+String.valueOf(progress));
+                for (TextView txtview : listToChange) {
+                    int newColor = ((ColorDrawable) txtview.getBackground()).getColor();
+                    //newColor = Color.rgb((Color.red(newColor) + (progress-previousProgress)*RGB_COLOR/100)%RGB_COLOR,(Color.green(newColor)+(progress-previousProgress)*RGB_COLOR/100)%RGB_COLOR, (Color.blue(newColor)+(progress-previousProgress)*RGB_COLOR/100)%RGB_COLOR) ;
+
+                    float[] hsvColor = {0, 1, 1};
+                    // generate only hue component in range [0, 360),
+                    // leaving saturation and brightness maximum possible
+                    Color.colorToHSV(newColor,hsvColor);
+                    hsvColor[0] += 360f * (progress) / 100;
+                    hsvColor[0] %=360;
+                    newColor =Color.HSVToColor(hsvColor);
+
+                    Log.i(TAG, "--->Id:" + String.valueOf(txtview.getId()) + ", Color:" + Integer.toHexString(newColor));
+
+                    txtview.setBackgroundColor(newColor);
+
                 }
+
+                //previousProgress = progress;    //this progress becomes previous progress!
             }
 
             @Override
@@ -68,6 +93,15 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        // Create a new AlertDialogFragment
+        mDialog = AlertDialogFragment.newInstance();
+
+        // Show AlertDialogFragment
+        mDialog.show(getFragmentManager(), "Alert");
+
+
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -76,5 +110,70 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static ArrayList<TextView> getTextViews(ViewGroup viewGroup)
+    {
+        ArrayList<TextView> list = new ArrayList<>();
+
+        if (viewGroup.getChildCount() == 0)
+            return list;
+        else
+        {
+            for (int index = 0; index < viewGroup.getChildCount();index ++)
+            {
+                View view = viewGroup.getChildAt(index);
+                if (view instanceof TextView) {
+                    TextView textView= (TextView) view;
+                    int textViewColor = ((ColorDrawable)textView.getBackground()).getColor();
+                    if (textViewColor != Color.WHITE && textViewColor != Color.GRAY)
+                        list.add(textView);
+                }
+                else if (view instanceof ViewGroup)
+                {
+                    list.addAll(getTextViews((ViewGroup)view));
+                }
+
+            }
+        }
+
+        return list;
+    }
+
+    // Class that creates the AlertDialog
+    public static class AlertDialogFragment extends DialogFragment {
+
+        public static AlertDialogFragment newInstance() {
+            return new AlertDialogFragment();
+        }
+
+        // Build AlertDialog using AlertDialog.Builder
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage("Click Below to learn more?")
+
+                            // User cannot dismiss dialog by hitting back button
+                    .setCancelable(false)
+
+                            // Set up No Button
+                    .setNegativeButton("Visit MOMA",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    Intent baseIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+                                    startActivity(baseIntent);
+                                }
+                            })
+
+                            // Set up Yes Button
+                    .setPositiveButton("Not Now",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        final DialogInterface dialog, int id) {
+                                   dismiss();
+                                }
+                            }).create();
+        }
     }
 }
